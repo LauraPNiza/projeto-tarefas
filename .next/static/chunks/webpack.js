63c5271b7ac42dd38a7f73b43fe3f7557bcf57d0
Laura Niza <92166534+LauraPNiza@users.noreply.github.com>
2023-06-22 20:34:28 -0300

    protegendo páginas
    se usuário não tiver login ele não acessa o dashboard

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "48266e54e7680ac3"; }
+/******/ 		__webpack_require__.h = function() { return "8eb313b33b3a513c"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -1,5 +1,8 @@
+import { GetServerSideProps } from 'next'
 import styles from './styles.module.css'
 import Head from 'next/head'
+import {getSession} from 'next-auth/react'
+import { redirect } from 'next/dist/server/api-utils'
 
 export default function Dashboard () {
     return(
@@ -14,3 +17,20 @@ export default function Dashboard () {
     )
 }
 
+export const getServerSideProps: GetServerSideProps = async ({req})=> {
+    const session = await getSession({req})
+    console.log(session)
+
+    if(!session?.user){
+        return{
+            redirect:{
+                destination: "/",
+                permanent: false,
+            },
+        }
+    }
+
+    return{
+        props: {},
+    }
+}
